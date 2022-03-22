f7e55b7a30764905b1a4bebeebbf5c9d8dd9ec85
Barbary-Theo <theo.barbary25@gmail.com>
2022-03-22 12:46:11 +0100

    login page front

---
+++ lib/loginPage.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  tempo() {
+    print("oui");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox (
+          width: MediaQuery.of(context).size.width / 1.2,
+          height: MediaQuery.of(context).size.height / 2.05,
+          child: Card(
+            shadowColor: Colors.black,
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            color: const Color(0xFFFFDDB6),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 24,
+                ),
+                const Center(
+                    child: Text("Bienvenu sur \n Done&Gone", style: TextStyle(fontSize: 25),)
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 10,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(40.0),
+                            ),
+                          ),
+                          filled: true,
+                          hintText: "Identifiant",
+                          fillColor: Color(0xFFFCFCFC)
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 30,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(40.0),
+                            ),
+                          ),
+                          filled: true,
+                          hintText: "Mot de passe",
+                          fillColor: Color(0xFFFCFCFC)
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 30,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Se connecter', style: TextStyle(color: Colors.black),),
+                    style: ButtonStyle(
+                      backgroundColor:  MaterialStateProperty.all<Color>(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    )
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 20,
+                ),
+                Divider(color: Colors.black, height: 1),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width / 30,
+                ),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("S'inscrire", style: TextStyle(color: Colors.black),),
+                      style: ButtonStyle(
+                        backgroundColor:  MaterialStateProperty.all<Color>(Color (0xFFD8D2ED)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      )
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'loginPage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,13 +26,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
